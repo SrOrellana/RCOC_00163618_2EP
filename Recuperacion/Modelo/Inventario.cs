841c95aedc805171cb9d8febe8b2265ec1566c97
SrOrellana <00163618@uca.edu.sv>
2020-05-31 10:54:58 -0600

    Recovery: Exam to recover a subject

---
+++ Recuperacion/Modelo/Inventario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preparcial.Modelo
+{
+    public class Inventario
+    {
+        //Poner public las propiedades
+        public string idArticulo { get; }
+        public string producto { get; }
+        public string descripcion { get; }
+        public string precio { get; }
+        public string stock { get; }
+
+        public Inventario(string idArticulo, string producto, string precio, string descripcion, string stock) //Cambiar orden de dos parametros "precio y descripcion"
+        {
+            this.idArticulo = idArticulo;
+            this.producto = producto;
+            this.descripcion = descripcion;
+            this.precio = precio;
+            this.stock = stock;
+        }
+    }
+}
---
+++ Recuperacion/Modelo/Usuario.cs
@@ -0,0 +1,21 @@
+﻿namespace Preparcial.Modelo
+{
+    public class Usuario
+    {
+        public string IdUsuario { get; set; }
+
+        public string NombreUsuario { get; set; }
+
+        public string Contrasena { get; set; }
+
+        public bool Admin { get; set; }
+
+        public Usuario(string idUsuario, string nombre, string contrasenia, bool admin)
+        {
+            IdUsuario = idUsuario;
+            NombreUsuario = nombre;
+            Contrasena = contrasenia;
+            Admin = admin;
+        }
+    }
+}
